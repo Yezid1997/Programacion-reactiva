714bb64aa380a763df0bbc0061525b281748c9cb
Yezid Buritica Luna <blak1997@gmail.com>
2026-09-21 22:27:06 -0500

    feat: first commit project was initialized

---
+++ src/test/java/com/shippingapp/shippingapp/ShippingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shippingapp.shippingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
